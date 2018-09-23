d4d520c6c32684a494bbc04f6cf24b72d6e8d9fb
Davide <davidemacchi97@gmail.com>
2018-09-23 22:28:02 +0200

    aggiornato

--- sketch.js
+++ sketch.js
@@ -7,18 +7,45 @@ function setup() {
 }
 
 function draw() {
-	background(204);
-fill(0,10,100);
+strokeWeight(2);
+	background(204,245,245);
+fill(255,255,50);
 
+//antenne
+	line(210, 165, 250, 245);
+		line(280, 165, 250, 245);
 	//Neck
 	line(250, 285, 250, 345);
-
+	//hands and arms
+	strokeWeight(2);
+		line(150, 285, 250, 440);
+			line(350, 285, 250, 440);
+strokeWeight(2);
 	//Wheel
-	ellipse(250, 465, 70, 70);
+	ellipse(285, 472, 55, 55);
+		ellipse(215, 472, 55, 55);
 
+		fill(5,255,50);
+		ellipse(285, 472, 40, 40);
+			ellipse(215, 472, 40, 40);
+
+
+
+
+fill(255,255,50);
 	//Body
-	rect(200, 325, 100, 140);
+	rect(195, 295, 110, 150);
 
 	//Head
 	ellipse(250, 240, 90, 90);
+
+	fill(205,255,250);
+	ellipse(265, 235, 20, 30);
+		ellipse(235, 235, 20, 30);
+		//eyes
+	strokeWeight(0);
+			fill(5,5,250);
+		ellipse(265, 240, 10, 10);
+				ellipse(235, 240, 10, 10);
+
 }
